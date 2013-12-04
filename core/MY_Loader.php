3f6fa4e4f4d59bb001f97d5b3c30225156148788
Dida Nurwanda <didanurwanda@gmail.com>
2013-12-04 10:27:51 +0700

    fix return view

--- core/MY_Loader.php
+++ core/MY_Loader.php
@@ -3,29 +3,34 @@
 require_once dirname(__FILE__) . '/../third_party/YiiIgniter/YiiIgniter.php';
 
 class MY_Loader extends CI_Loader {
-    
-    
+
     public function __construct() {
         parent::__construct();
         Yii::initialize();
     }
-    
+
     public function widget($alias = '', $data = []) {
         Yii::app()->widget->createWidget($alias, $data);
     }
-    
+
     public function beginWidget($alias = '', $data = []) {
         return Yii::app()->widget->beginWidget($alias, $data);
     }
-    
+
     public function end($alias = '') {
         Yii::app()->widget->end($alias);
     }
-    
+
     public function view($view, $vars = array(), $return = FALSE) {
         $view = parent::view($view, $vars, true);
         $search = array('{POS_HEAD}', '{POS_END}');
         $replace = array(Yii::app()->clientScript->posHead(), Yii::app()->clientScript->posEnd());
-        echo str_replace($search, $replace, $view);
+        $views = str_replace($search, $replace, $view);
+        if ($return === true) {
+            return $views;
+        } else {
+            echo $views;
+        }
     }
+
 }
